1d928e6fb1a91dd935dd44eeb5d0e3a9c1efb674
Nitrowritescode <swarsehgal123@gmail.com>
2025-02-15 00:36:47 +0530

    fixed blog page

---
+++ app/blog/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className=''>
+          <div className="bg-primary text-white py-16 text-center">
+        <h1 className="text-4xl font-bold">Blog Page</h1>
+        <p className="text-lg mt-4 max-w-3xl mx-auto">
+          Checkout Some Blogs 
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default page
