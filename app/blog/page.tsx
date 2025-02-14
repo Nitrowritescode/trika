894c496074fa67a3f354b3948b98f52147c6cee7
Nitrowritescode <swarsehgal123@gmail.com>
2025-02-15 00:42:06 +0530

    fixed blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function page() {
+function Blog() {
   return (
     <div className=''>
           <div className="bg-primary text-white py-16 text-center">
@@ -13,4 +13,4 @@ function page() {
   )
 }
 
-export default page
+export default Blog
